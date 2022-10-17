267fee41c51859013cdda0389dc16bafb98474c9
Madi <abaymadi1@gmail.com>
2022-10-17 16:59:21 +0600

    Мы добавили шаблон.
    Мы добавили header и footer, еще определили часть контента.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Privet Mir</h1>')
+    return render(request, 'blog/index.html')
